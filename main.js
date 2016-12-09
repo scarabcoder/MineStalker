f694fb6bd11c6fc6d4b987b71c62f3e3a5ebbeef
Nicholas <scarabcoder@gmail.com>
2016-12-09 15:40:16 -0800

    Updates

--- main.js
+++ main.js
@@ -2,30 +2,36 @@ var express = require('express')
 var app = express()
 var request = require("request")
 app.set('view engine', 'pug')
+app.use(express.static('views'))
+
+var getIDURL = "https://api.mojang.com/users/profiles/minecraft/";
+
+var getNames = "https://api.mojang.com/user/profiles/uuid/names";
+
+app.use('/views', express.static('views'))
+
 
 app.get('/user/*', function (req, res) {
-  var uuid = ""
 
-request({url: "https://api.mojang.com/users/profiles/minecraft/" + req.url.replace("/user/", ""), json: true}, function(error, response, body) {
-
-  uuid = body.id;
-  var url = "https://api.mojang.com/user/profiles/" + uuid + "/names"
-  console.log(url)
   request({
-      url: url,
-      json: true
+    url: getIDURL + req.url.replace("/user/", ""),
+    json: true
   }, function (error, response, body) {
 
       if (!error && response.statusCode === 200) {
-          console.log(body[0]) // Print the json response
-          res.render(
-              'index',
-              { title: 'Hey Hey Hey!', rows: body})
-      }else{
 
+        request({url: getNames.replace("uuid", body.id), json: true}, function (error, response, body2) {
+          body2.reverse();
+          res.render('index', { username: req.url.replace("/user/", ""), uuid: body.id, names: body2, isError: false})
+        })
+
+
+      }else{
+        res.render('index', { isError: true })
       }
   })
-})
+
+
 
 
 
